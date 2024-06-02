d3cc420b22920fab4bc3ccc07e646cfbfb669468
codexpression-nj <n.codexpression@gmail.com>
2024-06-02 13:45:00 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'workout',
+  webDir: 'www'
+};
+
+export default config;
